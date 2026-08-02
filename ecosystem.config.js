34284d51de4b83a0a5a7c46fbbc7905861a2b171
zuohang20100323 <zuohang20100323@qq.com>
2026-08-03 01:50:58 +0800

    ecosystem.config.js: pm2 managed, gateway only

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,18 +13,5 @@ module.exports = {
         PORT: 3000,
       },
     },
-    {
-      name: 'wake-up',
-      script: 'wake_up.js',
-      exec_mode: 'fork',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '128M',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-      },
-    },
   ],
 };
